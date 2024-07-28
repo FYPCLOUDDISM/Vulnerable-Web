95fb2f995977792bf8a0b19488199028a99f7f3f
FYPCLOUDDISM <168073511+FYPCLOUDDISM@users.noreply.github.com>
2024-07-28 15:56:45 +0800

    Update dbFunctions.php

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set('Singapore');
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "fypATK";
+$db_host = "fyprds.cvc45aykghor.us-east-1.rds.amazonaws.com";
+$db_user = "admin";
+$db_pass = "cloudfyp";
+$db_name = "cloud_fyp";
 $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die(mysqli_connect_error());
 
 // Create a secure database connection
